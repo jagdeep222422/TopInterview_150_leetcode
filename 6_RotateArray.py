d459a26694730893e8baca0cc9c56b5620ef2372
jagdeep222422 <76045872+jagdeep222422@users.noreply.github.com>
2023-09-23 00:00:36 +0530

    Create 6_RotateArray.py

---
+++ 6_RotateArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        if k==0:return 1
+        a=nums[:len(nums)-k]
+        b=nums[len(nums)-k:]
+        c=b+a
+        for num in range(0,len(nums)):
+           nums[num]=c[num]
