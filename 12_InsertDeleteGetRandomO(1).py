8dccf00a8b591a9d4d4fe280ee85948b7128c3a2
jagdeep222422 <76045872+jagdeep222422@users.noreply.github.com>
2023-09-29 22:11:57 +0530

    Create 12_InsertDeleteGetRandomO(1).py

---
+++ 12_InsertDeleteGetRandomO(1).py
@@ -0,0 +1,19 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.sets = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.sets : 
+            return False
+        self.sets.add(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.sets:
+            self.sets.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.sets))
